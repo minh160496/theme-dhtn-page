378db6db1bfe9bbbf102f38a5f13540d29a766e3
minh160496 <leminh16041996@gmail.com>
2023-09-15 11:38:33 -0700

    change getStaticProps

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -5,7 +5,44 @@ import { useDisclosure } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
 
+const Banner = dynamic(() => import("./Banner").then((mod) => mod.Banner), {
+  loading: () => <Loading />,
+});
+const Categorys = dynamic(
+  () => import("./Categorys").then((mod) => mod.Categorys),
+  {
+    loading: () => <Loading />,
+  }
+);
+const Benefit = dynamic(() => import("./Benefit").then((mod) => mod.Benefit), {
+  loading: () => <Loading />,
+});
+const Contact = dynamic(() => import("./Contact").then((mod) => mod.Contact), {
+  loading: () => <Loading />,
+});
+const Counters = dynamic(
+  () => import("./Counters").then((mod) => mod.Counters),
+  {
+    loading: () => <Loading />,
+  }
+);
 const Event = dynamic(() => import("./Event").then((mod) => mod.Event), {
+  loading: () => <Loading />,
+});
+
+const ModalBase = dynamic(
+  () => import("./Modal").then((mod) => mod.ModalBase),
+  {
+    loading: () => <Loading />,
+  }
+);
+const Review = dynamic(() => import("./Review").then((mod) => mod.Review), {
+  loading: () => <Loading />,
+});
+const Motto = dynamic(() => import("./Motto").then((mod) => mod.Motto), {
+  loading: () => <Loading />,
+});
+const Support = dynamic(() => import("./Support").then((mod) => mod.Support), {
   loading: () => <Loading />,
 });
 
@@ -18,5 +55,19 @@ export const Home = ({ posts }: { posts: any[] }) => {
     }, 1500);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <Event posts={posts} />;
+  return (
+    <>
+      <Banner />
+      <Categorys />
+      <Benefit />
+      <Motto />
+      <Support />
+      <Counters />
+      <Review />
+      <Contact />
+      <Event posts={posts} />
+
+      <ModalBase isOpen={isOpen} onClose={onClose} onOpen={onOpen} />
+    </>
+  );
 };
