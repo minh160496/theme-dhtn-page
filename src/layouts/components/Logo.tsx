0c0d95fce1d7c813bbf01dc4a75fd4a918e6ce3b
minh160496 <leminh16041996@gmail.com>
2023-09-15 17:46:25 -0700

    change logo

--- src/layouts/components/Logo.tsx
+++ src/layouts/components/Logo.tsx
@@ -6,9 +6,9 @@ export const Logo = () => {
     <Link href="/">
       <Image
         priority
-        width={80}
-        height={80}
-        src={`/logo-tuaf.png`}
+        width={100}
+        height={100}
+        src={`/logo.png`}
         alt="logo evstep"
       />
     </Link>
