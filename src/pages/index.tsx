8c126e3b30dfe6dd591d0fe8ebaedd863c328b8a
minh160496 <leminh16041996@gmail.com>
2023-09-16 09:15:17 -0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,33 @@ import { Loading } from "@/components/Loading";
 import { Home } from "@/features/home";
 
 export const getStaticProps = async () => {
-  const api_url = process.env.API_URL || "";
-  const res = await fetch(
-    `${api_url}/posts?_embed&per_page=3&status=publish&page=1`,
-    {
-      next: { revalidate: 3600 },
-    }
-  );
-  const errorCode = res.ok ? false : res.status;
+  try {
+    const api_url = process.env.API_URL || "";
+    const res = await fetch(
+      `${api_url}/posts?_embed&per_page=3&status=publish&page=1`,
+      {
+        next: { revalidate: 3600 },
+      }
+    );
 
-  const totalPosts = res.headers.get("X-WP-Total");
-  const posts = await res?.json();
+    const totalPosts = res.headers.get("X-WP-Total");
+    const posts = await res?.json();
 
-  const postsWithFeaturedImages: any[] = posts?.map((post: any) => {
-    const featured_image =
-      post._embedded?.["wp:featuredmedia"]?.[0]?.source_url || null;
+    const postsWithFeaturedImages: any[] = posts?.map((post: any) => {
+      const featured_image =
+        post._embedded?.["wp:featuredmedia"]?.[0]?.source_url || null;
 
-    return {
-      ...post,
-      featured_image,
-    };
-  });
+      return {
+        ...post,
+        featured_image,
+      };
+    });
 
-  return { props: { errorCode, posts: postsWithFeaturedImages, totalPosts } };
+    return { props: { posts: postsWithFeaturedImages, totalPosts } };
+  } catch (error) {
+    console.log(error);
+    return { props: { posts: null, totalPosts: 0 } };
+  }
 };
 
 interface IHomepage {
